290bdc499a2b80542757685426e89a68619c6201
Thomas Booth <thomasbooth26@gmail.com>
2022-11-28 01:12:07 +0000

    New files

---
+++ react-learn-proj/src/components/Button.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+const Button = ({onClick, color, text}) => {
+
+    return (
+        <button onClick = {onClick} 
+        style = {{backgroundColor: color}} 
+        className = "btn"> 
+            {text} 
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+    onClick: () => {
+        console.log("Clicked")
+    }
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+
+}
+export default Button
