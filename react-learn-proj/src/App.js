e25da131b8168fcef18b94d6fcfc3940a43fa6d2
Thomas Booth <thomasbooth26@gmail.com>
2022-11-28 01:09:37 +0000

    Task tracker -> Up to ondoubleclick

--- react-learn-proj/src/App.js
+++ react-learn-proj/src/App.js
@@ -1,12 +1,55 @@
 import './App.css';
 import Header from './components/Header.js';
 import React from 'react'
+import { useState } from 'react'
+import Tasks from './components/Tasks'
 
-function App() {
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Doctors Appointmment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text: 'Food Shopping',
+      day: 'Feb 6th at 3pm',
+      reminder: false,
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+      (task.id === id ? {...task, reminder: 
+        !task.reminder} : task)))
+    
+    console.log(id)
+  }
 
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks = {tasks} 
+        onDelete = {deleteTask} 
+        onToggle = {toggleReminder}/>
+      ) : (
+        'No Tasks To Show'
+      )} 
     </div>
   );
 }
